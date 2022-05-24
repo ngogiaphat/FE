6d764f4022d22d97c631b319e44590bf7b7ac83d
ngogiaphat <ngogiaphat0802@gmail.com>
2022-05-24 17:02:56 +0700

    test

--- JS/config.js
+++ JS/config.js
@@ -1,11 +1,10 @@
 
 const username = document.getElementById('username');
 const submit = document.getElementById('submit');
-
 submit.addEventListener('click', () => {
     var re_username = /^[a-zA-Z]{3,}$/;
-
-    if(!re_username.test(username)) {
+    if(!re_username.test(username)) 
+    {
         document.getElementById('error_username').innerHTML = "ngu";
         return false;
     }
